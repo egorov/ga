6860bccaf14bee21fd6f3998ecd77fd72e960fab
Sergey Egorov <me@egorov.space>
2021-09-19 22:12:03 +0500

    Переписал сортировку выбором без изменения массива с исходными данными

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,31 +1,55 @@
 module.exports = function selectionSort(values) {
 
-  const source = Array.from(values);
-  const count = values.length;
-  const result = [];
-  
-  for(let index = 0; index < count; index++){
-    const smallest = getSmallest(source);
-    result.push(smallest);
+  const state = {
+    iIndex: 0,
+    indices: new Array(values.length),
+    results: new Array(values.length),
+    rIndex: 0,
+    value: values[0],
+    vIndex: 0,
+    values
+  };
+
+  while(state.rIndex < state.values.length) {
+    getSmallest(state);
   }
 
-  return result;
+  return state.results;
 };
 
-function getSmallest(values) {
+function getSmallest(state) {
 
-  let value = values[0];
-  let value_index = 0;
+  setValue(state);
 
-  for(let index = 0; index < values.length; index++) {
+  for(let index = 0; index < state.values.length; index++) {
 
-    if(value > values[index]) {
-      value = values[index];
-      value_index = index;
-    }
+    if(state.indices.indexOf(index) !== -1) continue;
+
+    if(state.value <= state.values[index]) continue;    
+
+    state.value = state.values[index];
+    state.vIndex = index;
   }
 
-  values.splice(value_index, 1);
+  state.results[state.rIndex] = state.value;
+  state.rIndex += 1;
 
-  return value;
+  state.indices[state.iIndex] = state.vIndex;
+  state.iIndex += 1;
+}
+
+function setValue(state) {
+
+  state.value = void 0;
+
+  for(let index = 0; index < state.values.length; index++) {
+
+    if(state.indices.indexOf(index) !== -1) continue;
+  
+    if(state.results.indexOf(state.values[index]) !== -1) continue;
+
+    state.value = state.values[index];
+    state.vIndex = index;
+    break;
+  }
 }
