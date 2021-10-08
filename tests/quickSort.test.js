5e214d2d5264bdd83c1fbd861e24e98d89bd1b4a
Sergey Egorov <me@egorov.space>
2021-10-08 08:38:35 +0500

    Быстрая сортировка на основе рекурсии

---
+++ tests/quickSort.test.js
@@ -0,0 +1,15 @@
+describe('quickSort', () => {
+
+  const sort = require('../src/quickSort');
+  const digits = 
+    [25, 17, 6, 21, 9, 3, 1, 18, 26, 0, 7, 22, 5, 2, 20, 4, 19, 8, 24, 15, 11, 27, 14, 13, 12, 10, 16, 23];
+
+  it('should pass', () => {
+
+    const sorted = sort(digits);
+
+    for(let i = 0; i < sorted.length; i++) {
+      expect(sorted[i]).toEqual(i);
+    }
+  });
+});
